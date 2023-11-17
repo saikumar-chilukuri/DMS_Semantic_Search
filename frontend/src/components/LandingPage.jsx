b42ad2acf8f71505ad3304edf31c5ed71f5b8125
Sai Ch <nsaikumar.ch@gmail.com>
2023-11-17 14:45:31 +0530

    second update

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,36 +1,63 @@
 import React from 'react';
 
-function LandingPage() {
+const LandingPage = () => {
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:3000/auth/signin';
+  };
+
+  const handleSignUp = () => {
+    window.location.href = 'http://localhost:3000/auth/signup';
+  };
+
   return (
     <div
       className='flex'
       style={{
-        // background: 'red',
-        height: '100vh',
+        height: '90vh',
         justifyContent: 'center',
         alignItems: 'center',
       }}
     >
       <div>
-        Semantic Search - Document Management System (DMS)
+        <span style={{ fontSize: '28px' }}>
+          {' '}
+          Semantic Search - Document Management System (DMS)
+        </span>
         <div
           style={{
-            background: '#f0f0f0',
             padding: '20px',
           }}
         >
-          <ul className='flex items-center space-x-10'>
+          <ul className='flex justify-center items-center space-x-10'>
             <li>
-              <button>Login</button>
+              <button
+                style={{
+                  backgroundColor: '#c9c9c9',
+                  padding: '15px',
+                  fontWeight: 500,
+                }}
+                onClick={handleLogin}
+              >
+                Login
+              </button>
             </li>
             <li>
-              <button>SignUp</button>
+              <button
+                style={{
+                  backgroundColor: '#c9c9c9',
+                  padding: '15px',
+                  fontWeight: 500,
+                }}
+                onClick={handleSignUp}
+              >
+                SignUp
+              </button>
             </li>
           </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default LandingPage;
